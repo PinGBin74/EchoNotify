3b5e8386f2ea0702dd4894aedebe8defee069b75
Anton Fayfer <fayfer2004@mail.ru>
2025-11-26 18:23:15 +0600

    add user creation repository

--- echonotify/user/user_profile/repository.py
+++ echonotify/user/user_profile/repository.py
@@ -11,27 +11,28 @@ from echonotify.user.user_profile.models import UserProfile
 class UserRepository:
     db_session: AsyncSession
 
-    async def create_user(self, user_data: UserCreateSchema) -> UserProfile:
-        async with self.db_session as session:
-            user = UserProfile(
-                name=user_data.name,
-                email=user_data.email,
-                password=user_data.password,
-            )
-            session.add(user)
-            await session.commit()
-            await session.refresh(user)
-            return user
+    async def create_user(
+        self, session: AsyncSession, user_data: UserCreateSchema
+    ) -> UserProfile:
+        user = UserProfile(
+            name=user_data.name,
+            email=user_data.email,
+            password=user_data.password,
+        )
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+        return user
 
-    async def get_user_by_email(self, email: str) -> UserProfile:
-        async with self.db_session as session:
-            user_query = select(UserProfile).where(UserProfile.email == email)
-            result = await session.execute(user_query)
-            return result.scalar_one_or_none()
+    async def get_user_by_email(
+        self, session: AsyncSession, email: str
+    ) -> UserProfile:
+        user_query = select(UserProfile).where(UserProfile.email == email)
+        result = await session.execute(user_query)
+        return result.scalar_one_or_none()
 
-    async def get_user_by_id(self, user_id: int):
-        async with self.db_session as session:
-            user_query = select(UserProfile).where(UserProfile.id == user_id)
-            result = await session.execute(user_query)
-            user = result.scalar_one_or_none()
-            return user
+    async def get_user_by_id(self, session: AsyncSession, user_id: int):
+        user_query = select(UserProfile).where(UserProfile.id == user_id)
+        result = await session.execute(user_query)
+        user = result.scalar_one_or_none()
+        return user
