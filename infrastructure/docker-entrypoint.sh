653a421c1cab3c754078044698db2f8639178a76
Anton Fayfer <fayfer2004@mail.ru>
2026-01-02 18:50:34 +0600

    build: fix celery config

--- infrastructure/docker-entrypoint.sh
+++ infrastructure/docker-entrypoint.sh
@@ -1,6 +1,10 @@
-
 #!/bin/sh
 set -e
+
+if ! id -u appuser > /dev/null 2>&1; then
+    groupadd -g 1000 appgroup 2>/dev/null || true
+    useradd -u 1000 -g appgroup -s /bin/bash -m appuser 2>/dev/null || true
+fi
 
 if [ -n "$WAIT_FOR_POSTGRES" ] && [ "$WAIT_FOR_POSTGRES" = "true" ]; then
     echo "Waiting for PostgreSQL..."
